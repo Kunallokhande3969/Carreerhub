c993f8d1271b3d25462cbdd976800c3e8c9db8f5
kunal lokhande <kunallokhande3969@gmail.com>
2025-07-31 02:39:35 -0900

    first commit

--- Back-end/utiles/SendTokens.js
+++ Back-end/utiles/SendTokens.js
@@ -6,9 +6,10 @@ exports.sendtoken = (employe, statusCode, res) => {
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    // secure : true
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   };
-  //  
+
   res
     .status(statusCode)
     .cookie("token", token, options)
