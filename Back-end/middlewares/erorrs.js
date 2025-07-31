4e1f4b45d4dc89c812304c05f7e3cc294da91cfc
kunal lokhande <kunallokhande3969@gmail.com>
2025-07-31 01:18:15 -0900

    first commit

--- Back-end/middlewares/erorrs.js
+++ Back-end/middlewares/erorrs.js
@@ -1,17 +1,19 @@
 exports.generatedErorrs = (err, req, res, next) => {
-  console.log("Error: ", err);
+  console.error("Error: ", err);  
   const statusCode = err.statusCode || 500;
 
   if (
     err.name === "MongoServerError" &&
     err.message.includes("E11000 duplicate key")
   ) {
-    err.message = "User with this email address is already exist";
+    err.message = "User with this email address already exists";
+    
   }
 
   res.status(statusCode).json({
+     
     message: err.message,
     errName: err.name,
-    stack: err.stack,
+    stack: err.stack,  
   });
 };
